18af81b21a8ac75afc2ca6a1adc420a4bf9a8a0d
faradizalovy <lovyfaradiza@gmail.com>
2026-02-10 12:38:54 +0700

    Pemilihan No 1

---
+++ pemilihan09.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class pemilihan09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Program Menghitung Nilai Akhir");
+        System.out.println("=================================");
+
+        System.out.print("Masukkan Nilai Tugas: ");
+        double tugas = sc.nextDouble();
+        System.out.print("Masukkan Nilai Kuis: ");
+        double kuis = sc.nextDouble();
+        System.out.print("Masukkan Nilai UTS: ");
+        double uts = sc.nextDouble();
+        System.out.print("Masukkan Nilai UAS: ");
+        double uas = sc.nextDouble();
+
+        System.out.println("==================================");
+        System.out.println("==================================");
+
+        boolean valid = true;
+        if (tugas < 0 || tugas > 100) valid = false;
+        if (kuis < 0 || kuis > 100) valid = false;
+        if (uts < 0 || uts > 100) valid = false;
+        if (uas < 0 || uas > 100) valid = false;
+
+        if (valid == false) {
+            System.out.println("nilai tidak valid");
+            System.out.println("==============================");
+            System.out.println("==============================");
+
+    } else {
+        double nilaiAkhir = (0.2 * tugas) + (0.2 * kuis) + (0.3 * uts) + (0.4 * uas);
+        String nilaiHuruf = "";
+    
+        if (nilaiAkhir >= 80) {
+            nilaiHuruf = "A";
+        } else if (nilaiAkhir >= 73) {
+            nilaiHuruf = "B+";
+        } else if (nilaiAkhir >= 65) {
+            nilaiHuruf = "B";
+        } else if (nilaiAkhir >= 60) {
+            nilaiHuruf = "C+";
+        } else if (nilaiAkhir >= 50) {
+            nilaiHuruf = "C";
+        } else if (nilaiAkhir >= 39) {
+            nilaiHuruf = "D";
+        } else {
+            nilaiHuruf = "E";
+        }
+
+        System.out.println("Nilai akhir: " + nilaiAkhir);
+        System.out.println("Nilai huruf: " + nilaiHuruf);
+
+        System.out.println("==========================================");
+        System.out.println("==========================================");
+
+        if (nilaiHuruf.equals("D") || nilaiHuruf.equals("E")) {
+            System.out.println("ANDA TIDAK LULUS");
+        } else {
+            System.out.println("SELAMAT ANDA LULUS");
+        }
+    }
+    sc.close();
+    }
+}
